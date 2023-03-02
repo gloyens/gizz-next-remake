e490c3c15a54f55e0e8c9046f837ca2aec7f85a8
George <georgegloyens@gmail.com>
2023-03-02 11:30:35 +0000

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import "@/styles/globals.css"
+import "@/styles/colors.css"
 
 export const metadata = {
   title: 'Create Next App',
---
+++ src/app/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from "@phntms/css-components";
+
+import css from "./page.module.css";
+
+export const Container = styled("div", {
+  css: css.Container,
+});
